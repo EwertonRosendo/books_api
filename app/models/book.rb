c8062c9ce93de708d6321ce5b91ace38dd3d2d47
ewertonrosendo <ewerton.rosendo15@gmail.com>
2024-07-21 23:59:56 -0300

    add coverage and remove files

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,8 @@
 class Book < ApplicationRecord
   belongs_to :author
 
+  has_many :reviews, class_name: "Review", dependent: :destroy
+
   validates :title, presence: true
   validates :author, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
-class UsersBook < ApplicationRecord
+class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
-
   enum status: {
     to_read: 0,
     reading: 1,
