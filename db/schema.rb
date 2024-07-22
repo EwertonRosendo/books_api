c04fdd97ec3c58d8a5f58044cb56c8c699e1a0c8
ewertonrosendo <ewerton.rosendo15@gmail.com>
2024-07-22 15:05:01 -0300

    change books request to json format

--- db/schema.rb
+++ db/schema.rb
@@ -40,42 +40,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_17_183820) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "noticed_events", force: :cascade do |t|
-    t.string "type"
-    t.string "record_type"
-    t.bigint "record_id"
-    t.json "params"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "notifications_count"
-    t.index ["record_type", "record_id"], name: "index_noticed_events_on_record"
-  end
-
-  create_table "noticed_notifications", force: :cascade do |t|
-    t.string "type"
-    t.bigint "event_id", null: false
-    t.string "recipient_type", null: false
-    t.bigint "recipient_id", null: false
-    t.datetime "read_at", precision: nil
-    t.datetime "seen_at", precision: nil
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_noticed_notifications_on_event_id"
-    t.index ["recipient_type", "recipient_id"], name: "index_noticed_notifications_on_recipient"
-  end
-
-  create_table "notifications", force: :cascade do |t|
-    t.string "recipient_type", null: false
-    t.integer "recipient_id", null: false
-    t.string "type", null: false
-    t.json "params"
-    t.datetime "read_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["read_at"], name: "index_notifications_on_read_at"
-    t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
-  end
-
   create_table "reviews", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "book_id", null: false
