54e08675082bd37293f05cefea3c8b5f4674f0b8
ewertonrosendo <ewerton.rosendo15@gmail.com>
2024-07-22 00:56:42 -0300

    restore lines

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
-RSpec.describe Book, type: :model do
+RSpec.describe Book do
   author = Author.where(name: "Machado de Assis").first_or_create!
   book = Book.new(
     title: "Dom Casmurro",
