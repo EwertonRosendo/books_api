1d355203dde7c74e9dfefeb36f7ebd985c0f5724
ewertonrosendo <ewerton.rosendo15@gmail.com>
2024-08-02 13:28:43 -0300

    change url request for google books

--- app/javascript/components/GoogleBooks/GoogleBooks.js
+++ app/javascript/components/GoogleBooks/GoogleBooks.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
 import SearchBook from "./SearchBook";
 import ListBook from "./ListBooks";
-import PreLoader from "./PreLoader";
 
 const GoogleBooks = (props) => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    //const url = "http://34.195.216.47:3000/google-books/ruby";
-    const url = "http://localhost:3000/Googlebooks.json";
+    const url = "http://34.195.216.47:3000/google-books/";
     fetch(url)
       .then((res) => {
         if (res.ok) {
@@ -21,7 +19,6 @@ const GoogleBooks = (props) => {
 
   return (
     <React.Fragment>
-      {/*<PreLoader />*/}
       <SearchBook booksList={setBooks} />
       <ListBook books={books} />
     </React.Fragment>
